41150c2447aa11b6104003c8347270ffab4232b0
Katie Hughes <katiehughes@Katies-MacBook-Air-2.local>
2019-12-29 12:10:52 -0800

    board print

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+def printboard(board):
+	size = len(board)
+	lines = np.sqrt(size)
+	for x in range(0, size):
+		if np.mod(x, lines) == 0: 
+			print(" "), 
+			for z in range(0, size): 
+				print("__ "),
+			print("\n"), 
+		for y in range(0, size):
+			num = board[x][y]
+			if np.mod(y, lines) == 0: ###y == 0:
+				print("|"),
+			else:
+				print(" "),
+			print(num),
+		print("\n"), 
+
 
 def main():
 	print "Welcome to my sudoku solver!"
@@ -10,8 +28,12 @@ def main():
 		print "size is not a square"
 
 	###creating the board
-	board = np.zeros((size, size))
+	board = np.zeros((size, size), dtype=int)
 	print board
+	printboard(board)
+
+
+	##entering known values to board
 
 
 
